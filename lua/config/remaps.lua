23ff1711d31ec537e00dd59710cb9d48d7c97250
Tomasz Bawor <bawortomasz@gmail.com>
2025-10-10 22:49:08 +0200

    add change window remaps that I am used to

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -7,6 +7,13 @@ local non_lsp_mappings = {
     { "<leader>p", '"_dP', desc = "Paste without overwrite" },
     { "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", desc = "Search and replace word under cursor" },
     { "Q", "<nop>", desc = "Disable Ex mode" },
+
+  -- Window Management  
+    {"<C-h>", "<C-w>h", desc = "Move to the left window"},
+    {"<C-l>", "<C-w>l", desc = "Move to the right window"},
+    {"<C-j>", "<C-w>j", desc = "Move to the down window"},
+    {"<C-k>", "<C-w>k", desc = "Move to the top window"},
+
   }
 
 
