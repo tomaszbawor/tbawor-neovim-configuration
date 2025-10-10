16e34d2c8b50ebda5e1bea63b74de1fb8cad4048
Tomasz Bawor <bawortomasz@gmail.com>
2025-10-10 22:02:01 +0200

    add some shortcuts for telescope to match previous lazyvim config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = {'nvim-lua/plenary.nvim'}
+	dependencies = {'nvim-lua/plenary.nvim'},
+  opts = {},
+  keys = { 
+    { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    { "<leader>fg", function() require("telescope.builtin").live_grep() end,  desc = "Live grep" },
+    { "<leader>fb", function() require("telescope.builtin").buffers() end,    desc = "Buffers" },
+    { "<leader>fr", function() require("telescope.builtin").oldfiles() end,   desc = "Recent files" },
+    { "<leader>fh", function() require("telescope.builtin").help_tags() end,  desc = "Help" },
+  }
+
 }
