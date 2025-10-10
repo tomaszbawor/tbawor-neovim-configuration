2c2c17337dd36d190f24ea582088d3df6f0f975d
Tomasz Bawor <bawortomasz@gmail.com>
2025-10-10 23:17:09 +0200

    qol changes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,7 +1,14 @@
 return {
-        "nvim-tree/nvim-tree.lua",
-        opts = { on_attach = on_attach_change },
-    keys = {
-      {  "<leader>e", ":NvimTreeToggle <CR>", desc = "Open file explorer" },
-    }
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  opts = {
+    on_attach = on_attach_change,
+    renderer = {
+      group_empty = true,
+      highlight_git = true,
+    },
+  },
+  keys = {
+    { "<leader>e", ":NvimTreeToggle <CR>", desc = "Open file explorer" },
+  },
 }
