1934e08d48f83af2154d20f14e9001a48b7c9002
Tomasz Bawor <tomasz.bawor@tesco.com>
2025-10-13 13:47:54 +0200

    update some terminal maps

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,6 +5,8 @@ return {
   lazy = false,
   keys = {
     { "<leader>ft", "<Cmd>ToggleTerm direction=float<CR>", desc = "Toggle floating terminal" },
+    { "<leader>tt", "<Cmd>ToggleTerm<CR>", desc = "Toggle regular terminal" },
+    { "<leader>tt", "<Cmd>ToggleTerm<CR>", desc = "Toggle regular terminal", mode = "t" },
     -- <C-_> Means Control + / in neovim. I do not know why.
     { "<C-_>", "<Cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
     { "<C-_>", "<Cmd>ToggleTerm<CR>", mode = "t", desc = "Toggle terminal" },
